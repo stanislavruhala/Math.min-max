6f11f8e0e22915fb4ab63a81a75a6630b944554c
stanislavruhala <stanislav.ya.ruhala@ukd.edu.ua>
2025-11-25 00:49:28 +0200

    Add min-max calculation in math.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Math.min-max
---
+++ math.js
@@ -0,0 +1,7 @@
+const arr = [5, 12, -4, 7, 0];
+
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+
+console.log("Min:", min);
+console.log("Max:", max);
